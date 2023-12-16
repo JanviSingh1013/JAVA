77a76c8166913713bae8171e285a43d715d06d97
Shivansh Kumar <codes.stalker@gmail.com>
2023-12-16 19:00:29 +0530

    Create InvertedStarPattern.java

---
+++ InvertedStarPattern.java
@@ -0,0 +1,17 @@
+public class InvertedStarPattern{
+	public static void main(String[] args){
+		for(int i = 0; i < 5; i++){ 
+			for(int j = 0; j < 5 - i ;j++ ){
+				System.out.print("*" + " ");				
+			}
+			System.out.println();
+		}
+	}
+} 
+
+
+//* * * * * 
+//* * * * 
+//* * * 
+//* * 
+//*
