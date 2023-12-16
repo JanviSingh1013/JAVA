953f28710d52f57f67810bb937dfb92a564e6dda
Shivansh Kumar <codes.stalker@gmail.com>
2023-12-16 14:21:53 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,30 +2,32 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class BubbleSort{
 	public static void main(String[] args){
-		int[] arr = {42, 17, 98, 23, 55, 72, 31, 88, 10, 5, 67, 19, 81, 29, 93, 2, 49, 12, 77, 34, 53, 68, 9};
+		int[] arr = {1,2,3,4,5,6,7,8,9};
 ;
 		System.out.println(Arrays.toString(bubbleSort(arr)));
 	}
 
 	static int[] bubbleSort(int[] arr){
 		int pass = 0;
+		boolean swapped;
 		for(int i = 0;i<arr.length ;i++){
+			swapped = false;
 			for(int j = 1 ;j <= arr.length - i - 1;j++){
 				if(arr[j] < arr[j-1]){
 					int temp = arr[j];
 					arr[j] = arr[j-1];
 					arr[j-1] = temp;
+					swapped = true;
 				}
+
 				System.out.println(Arrays.toString(arr));
+			}
+			if(!swapped){
+				break;
 			}
 			System.out.println("Pass = " + pass++);
 		}
 		return arr;
 	}
 
-	// static void swap(int a, int b){
-	// 	int temp = a;
-	// 	a = b;
-	// 	b = temp;
-	// }
 }
