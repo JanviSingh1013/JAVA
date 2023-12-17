ae0f10e69c8ac73c6f2cbcfc7efac0ff2684513c
Shivansh Kumar <codes.stalker@gmail.com>
2023-12-17 15:26:44 +0530

    Answer to "Waht is commad line arguments  in java ?"

---
+++ Java_CommandLineArgumentTutorial.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class Java_CommandLineArgumentTutorial{
+	public static void main(String[] args){
+        System.out.println(args.length);
+        System.out.println(Arrays.toString(args));
+
+        /**
+         * To use the command line arguments do the following commands
+         * 
+         * 1. javac CommandLineArgumentTutorial.java // Click enter after it, then
+         * 2. java CommandLineArgumentTutorial A B C D E // A B C D E  are the the values 
+         * 												 //	we are providing during the commands 
+         * 											     //	we are giving to command promp
+         **/
+	}
+}
