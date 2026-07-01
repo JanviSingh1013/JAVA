282b4ca210a0926f7c07b5f321397eebbe281dea
Janvi Singh <115346755+JanviSingh1013@users.noreply.github.com>
2026-07-01 22:25:48 +0530

    Create ProductOfArrayExceptSelf.java

---
+++ ProductOfArrayExceptSelf.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int [] ans = new int[nums.length];
+        ans[0] = 1;
+        for(int i = 1; i < nums.length; i++){
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+        int suffix = 1;
+        for(int i = nums.length - 1; i >= 0; i--){
+            ans[i] = ans[i] * suffix;
+            suffix = suffix * nums[i];
+        }
+        return ans;
+    }
+}
